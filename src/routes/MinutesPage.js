1f1cb4ca84abe0f6368666b0cef8770679c63402
empressdaybreak <as616111@gmail.com>
2022-06-16 17:00:44 +0900

    feature: 회의록에 modal을 추가하고, 누른 값을 받을 수 있도록 함

--- src/routes/MinutesPage.js
+++ src/routes/MinutesPage.js
@@ -2,9 +2,6 @@ import { addDoc, collection, onSnapshot, query } from "firebase/firestore";
 import moment from "moment";
 import React, { useEffect, useState } from "react";
 import { dbService } from "../fbase";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBook } from "@fortawesome/free-solid-svg-icons";
-import { faPen } from "@fortawesome/free-solid-svg-icons";
 import styled from "styled-components";
 
 const ArticleBox = styled.div`
@@ -56,10 +53,63 @@ const ArticleCard = styled.div`
     }
 `;
 
+const ModalAlert = styled.div`
+    width: 100%;
+    height: 100%;
+    background: rgba(0,0,0,0.5);
+
+    position: fixed;
+    left: 0;
+    top: 0;
+    z-index: 9999;
+
+    & > div {
+        width: 700px;
+        height: 700px;
+        background: #fff;
+        
+        position: absolute;
+        left: 50%;
+        top: 50%;
+
+        margin-left: -350px;
+        margin-top: -350px;
+        padding: 15px;
+
+        box-sizing: border-box;
+        border-radius: 5px;
+
+        & p {
+            margin: 0;
+        }
+
+        & > div:first-child {
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+
+            padding: 20px 5px;
+        }
+
+        & > div:last-child {
+            height: 100%;
+
+            border: none;
+            box-shadow: 0 2px 10px rgb(0 0 0 / 10%);
+            border-radius: 5px;
+            padding: 10px;
+        }
+    }
+`;
+
 const MinutesPage = ({ userObj }) => {
     const date = moment().format("YYYY-MM-DD");
     const [textVal, setTextVal] = useState("");
     const [data, setData] = useState([]);
+
+    const [modalData, setModalData] = useState("");
+    const [modalDay, setModalDay] = useState("");
+    const [modalToggl, setModalToggle] = useState(false);
 
     const handleSetValue = (event) => {
         setTextVal(event.target.value);
@@ -69,6 +119,7 @@ const MinutesPage = ({ userObj }) => {
         event.preventDefault();
 
         const itemObj = {
+            index: data.length,
             content: textVal,
             writer: userObj.displayName,
             addDay: date,
@@ -78,6 +129,12 @@ const MinutesPage = ({ userObj }) => {
         setTextVal("");
     };
     
+    const modalNumberSend = (content, addDay) => {        
+        setModalData(content);
+        setModalDay(addDay);
+        setModalToggle(true);
+    };
+
     useEffect(() => {
         const q = query(collection(dbService, "minutes"));
 
@@ -107,21 +164,30 @@ const MinutesPage = ({ userObj }) => {
 
             <ArticleBox>
                 {data.map((data, index) => (
-                    <ArticleCard key={index}>
+                    <ArticleCard key={index} onClick={ () => modalNumberSend(data.content, data.addDay) }>
                         <div>
-                            <p>{data.addDay} 회의록</p>
-                            <FontAwesomeIcon icon={faBook} />
-                        </div>
-
-                        <p>{data.content}</p>
-
-                        <div>
-                            <FontAwesomeIcon icon={faPen} />
+                            <p>{data.addDay} 회의록</p>    
                             <span>{data.writer}</span>
                         </div>
+                        {/* <p>{data.content}</p> */}                        
                     </ArticleCard>
                 ))}
             </ArticleBox>
+
+            {modalToggl &&
+                <ModalAlert>
+                    <div>
+                        <div>
+                            <p>{modalDay} 회의록</p>
+                            <p onClick={() => setModalToggle(false)}>닫기</p>
+                        </div>
+
+                        <div>
+                            <p>{modalData}</p>
+                        </div>
+                    </div>
+                </ModalAlert>
+            }
         </>
     );
 };
