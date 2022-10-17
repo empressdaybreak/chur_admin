4fbb4adf3077d37fc83910c286affa3f0c187faf
daybreak <as616111@gmail.com>
2022-10-17 17:25:27 +0900

    fix: 웹훅 내용 변경 및 보안을 위해 웹훅 주소를 분리함

--- src/routes/ProposalPage.js
+++ src/routes/ProposalPage.js
@@ -9,115 +9,6 @@ import { faCat, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { faShieldCat } from "@fortawesome/free-solid-svg-icons";
 import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 
-const TodoContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-`;
-
-const TodoList = styled.div`
-    width: 25%;
-
-    padding: 25px;
-
-    &:not(:last-child) {
-        margin-right: 10px;
-    }
-
-    & > p {
-        margin: 0 0 10px;
-    }
-`;
-
-const TodoListHeader = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-
-    border-bottom: 3px solid #fff;
-    padding-bottom: 10px;
-    margin-bottom: 20px;
-
-    & p {
-        font-size: 25px;
-    }
-`;
-
-const Card = styled.div`
-    border: none;
-    box-shadow: 0 2px 10px rgb(0 0 0 / 10%);
-    border-radius: 5px;
-    
-    background: #fff;
-    color: #000;
-
-    padding: 15px;
-
-    &:not(:last-child) {
-        margin-bottom: 15px;
-    }
-`;
-
-const TextArea = styled.textarea`
-    border: 1px solid #dadada;
-    border-radius: 5px;
-    resize: none;
-
-    width: 100%;
-
-    padding: 10px;
-    box-sizing: border-box;
-
-    &:focus {
-        outline: none;
-    }
-`;
-
-const FlexBox = styled.div`
-    display: flex;
-    flex-direction: column;
-`;
-
-const ButtonArea = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-end;
-    & button {
-        border-radius: 5px;
-        color: #fff;
-        padding: 5px 10px;
-        border: none;
-        margin-top: 10px;
-
-        &:first-child {
-            background-color: skyblue;    
-            margin-right: 10px;
-        }
-
-        &:last-child {
-            background-color: red;
-        }
-    }
-`;
-
-const AlertDesc = styled.p`
-    text-align: center;
-    font-size: 18px;
-
-    padding: 30px 0;
-`;
-
-const ActiveStatus = styled.span`
-    height: 10px;
-    width: 10px;
-
-    border-radius: 10px;
-
-    margin-right: 5px;
-    
-    display: inline-block;
-`;
-
 const ProposalPage = ({ userObj }) => {
     const date = moment().format("YYYY-MM-DD");
     const [newData, setNewData] = useState([]);
@@ -155,12 +46,12 @@ const ProposalPage = ({ userObj }) => {
         // discord webhook 전송 (새 건의 사항이 있을때만 트리거 됨)
         const request = new XMLHttpRequest();
 
-        request.open("post", "https://discord.com/api/webhooks/1031428285424488478/Y58g61-ephCBSQAy6_cXYXZFgAJzJiXtdIcmOf_TNZqeDoNJm_CgqOJJ0aJE0xgn6_ef");
+        request.open("post", `${process.env.REACT_APP_WEBHOOK_URL}`);
         request.setRequestHeader('Content-type', 'application/json');
 
         const myEmbed = {
-            title: "새로운 알림!",
-            description: '"' + userObj.displayName.replace(process.env.REACT_APP_USERAUTH_TAG, '') + '" 님이 새로운 건의사항을 등록하였다냥! \n https://empressdaybreak.github.io/chur_admin/#/proposal',
+            title: "'" + userObj.displayName.replace(process.env.REACT_APP_USERAUTH_TAG, '') + "' 님이 새로운 건의사항을 등록하였다냥!",
+            description: '건의사항 내용: "' + inputTodo + '" \n https://empressdaybreak.github.io/chur_admin/#/proposal',
             color: hexToDecimal("#58b9ff"),
         }
 
@@ -357,5 +248,115 @@ const ProposalPage = ({ userObj }) => {
         </TodoContainer>
     );
 };
+
+
+const TodoContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+`;
+
+const TodoList = styled.div`
+    width: 25%;
+
+    padding: 25px;
+
+    &:not(:last-child) {
+        margin-right: 10px;
+    }
+
+    & > p {
+        margin: 0 0 10px;
+    }
+`;
+
+const TodoListHeader = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+
+    border-bottom: 3px solid #fff;
+    padding-bottom: 10px;
+    margin-bottom: 20px;
+
+    & p {
+        font-size: 25px;
+    }
+`;
+
+const Card = styled.div`
+    border: none;
+    box-shadow: 0 2px 10px rgb(0 0 0 / 10%);
+    border-radius: 5px;
+    
+    background: #fff;
+    color: #000;
+
+    padding: 15px;
+
+    &:not(:last-child) {
+        margin-bottom: 15px;
+    }
+`;
+
+const TextArea = styled.textarea`
+    border: 1px solid #dadada;
+    border-radius: 5px;
+    resize: none;
+
+    width: 100%;
+
+    padding: 10px;
+    box-sizing: border-box;
+
+    &:focus {
+        outline: none;
+    }
+`;
+
+const FlexBox = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+const ButtonArea = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    & button {
+        border-radius: 5px;
+        color: #fff;
+        padding: 5px 10px;
+        border: none;
+        margin-top: 10px;
+
+        &:first-child {
+            background-color: skyblue;    
+            margin-right: 10px;
+        }
+
+        &:last-child {
+            background-color: red;
+        }
+    }
+`;
+
+const AlertDesc = styled.p`
+    text-align: center;
+    font-size: 18px;
+
+    padding: 30px 0;
+`;
+
+const ActiveStatus = styled.span`
+    height: 10px;
+    width: 10px;
+
+    border-radius: 10px;
+
+    margin-right: 5px;
+    
+    display: inline-block;
+`;
 
 export default ProposalPage;
